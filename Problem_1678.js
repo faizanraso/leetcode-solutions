6d46ffafe510a7c122650393ba5906d6994529e1
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2022-07-31 19:28:06 -0400

    Problem 1678 Completed

---
+++ Problem_1678.js
@@ -0,0 +1,15 @@
+const command = "G()(al)"
+
+var interpret = function(command) {
+    var interpretation = ""
+    for(var i=0; i<command.length(); i++){
+        if(command[i] == "G"){
+            interpretation += "G"
+        } else if (command[i] == "(" && command[i+1] == ")"){
+            interpretation += "o"
+        } else if(command[i] == "(" && command[i+1] == "a" && command[i+2] == "l" && command[i+3] == ")"){
+            interpretation += "al"
+        }
+    }
+    return interpretation;
+};
