f041c47652024faed703bd1a9fd425899b6353bb
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2022-07-31 19:31:21 -0400

    Added alternative method for Problem 1678

--- Problem_1678.js
+++ Problem_1678.js
@@ -1,15 +1,21 @@
-const command = "G()(al)"
-
+//Initial Method
 var interpret = function(command) {
-    var interpretation = ""
+    var interpretation = "";
     for(var i=0; i<command.length(); i++){
         if(command[i] == "G"){
-            interpretation += "G"
+            interpretation += "G";
         } else if (command[i] == "(" && command[i+1] == ")"){
-            interpretation += "o"
+            interpretation += "o";
         } else if(command[i] == "(" && command[i+1] == "a" && command[i+2] == "l" && command[i+3] == ")"){
-            interpretation += "al"
+            interpretation += "al";
         }
     }
     return interpretation;
 };
+
+//Alternative Method
+var interpret = function(command){
+    command = command.replaceAll("()", "o");
+    command = command.replaceAll("(al)", "al");
+    return command;
+}
