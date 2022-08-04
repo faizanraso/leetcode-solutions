1f9a37dafc92688b24038dcf286819147e49a0d4
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2022-08-04 11:36:59 -0400

    Problem 19 solved

---
+++ Problem_19.js
@@ -0,0 +1,27 @@
+// Remove the Nth node from the end of a linked list
+
+var removeNthFromEnd = function(head, n) {
+    var length = 0;
+    let counterNode = head;
+    let soln = new ListNode();
+    let solnHead = soln;
+    
+    while(true){
+        length += 1;
+        if(!counterNode.next){break;}
+        else{
+            counterNode = counterNode.next;
+        }
+    }
+    
+    for(i = 1; i<=length; i++){
+        if(i != length-n+1){
+            soln.next = new ListNode(head.val);
+            soln = soln.next;
+            head = head.next;
+        } else {
+            head = head.next;
+        }
+    }
+    return solnHead.next
+};
