f1c60eb600f69935fe5d65f2b82be6d0c20dc30b
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-01-16 21:28:58 -0500

    Problem 278

---
+++ Problem_278.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var start = 1;
+        var end = n;
+        while(start <= end){
+            var curr = Math.floor((end-start)/2 + start);
+            if(isBadVersion(curr)) end = curr-1;
+            else start = curr+1;
+        }
+        return start;
+    };
+};
