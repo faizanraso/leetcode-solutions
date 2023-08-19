7345e70feb9b2c036059f9def5b3bdcc8721eb63
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-08-19 15:26:14 -0400

    prob added

---
+++ Problem_2626.ts
@@ -0,0 +1,13 @@
+type Fn = (accum: number, curr: number) => number;
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  if (nums.length == 0) return init;
+  else {
+    var val = fn(init, nums[0]);
+    for (let i = 1; i < nums.length; i++) {
+      val = fn(val, nums[i]);
+    }
+    return val;
+  }
+}
+
