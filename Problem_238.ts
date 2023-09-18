4b22420419da91e9acd0a0ee8ac1f61441c9d7c0
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-09-18 12:15:01 -0400

    better answer

--- Problem_238.ts
+++ Problem_238.ts
@@ -13,3 +13,28 @@ function productExceptSelf(nums: number[]): number[] {
     }
     return productArr
 };
+
+function productExceptSelf(nums: number[]): number[] {
+    let prefix: number[] = Array(nums.length).fill(1)
+    let postfix: number[] = Array(nums.length).fill(1)
+    
+    for(let i = 0; i < nums.length; i++){
+        if(i == 0) prefix[i] = nums[i];
+        else prefix[i] = nums[i] * prefix[i-1];
+    }
+
+    for(let i = nums.length - 1; i >= 0; i--){
+        if(i == nums.length - 1) postfix[i] = nums[i];
+        else postfix[i] = nums[i] * postfix[i+1];
+    }
+
+    let output: number[] = [];
+
+    for(let i = 0; i < nums.length; i++){
+        if(i == 0 ) output.push(1*postfix[1]);
+        else if(i == nums.length - 1) output.push(prefix[i - 1] * 1);
+        else output.push(prefix[i-1]*postfix[i+1]);
+    }
+
+    return output;
+};  
