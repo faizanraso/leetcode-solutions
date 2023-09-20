30c94c1675cc0b6defc458c819351d97da779614
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-09-20 18:55:24 -0400

    soln added

--- Problem_334.ts
+++ Problem_334.ts
@@ -8,3 +8,18 @@ function increasingTriplet(nums: number[]): boolean {
     }
     return false
 };
+
+function increasingTriplet(nums: number[]): boolean {
+    if(nums.length < 3) return false
+
+    let firstMin: number = Infinity;
+    let secondMin: number = Infinity;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] <= firstMin) firstMin = nums[i];
+        else if(nums[i] <= secondMin) secondMin = nums[i];
+        else return true;
+    }
+    
+    return false;
+};
