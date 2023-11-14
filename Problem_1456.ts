9b1e1cfce5118d06f39e87929ce30d917f3600c9
Faizan Rasool <faizanraso@outlook.com>
2023-11-14 18:57:37 -0500

    problem added

---
+++ Problem_1456.ts
@@ -0,0 +1,26 @@
+function isVowel(s: string){
+    const vowels = new Set(["a", "e", "i", "o", "u"]);
+    if(vowels.has(s)) return true;
+    return false
+}
+
+function maxVowels(s: string, k: number): number {
+
+    let stringArray = s.split('');
+    let maxVowels: number = 0;
+
+    for(let i = 0; i < k; i++){
+        if(isVowel(stringArray[i])) maxVowels ++;
+    }
+
+    if (maxVowels === k) return k;
+
+    let currWindowVowels: number = maxVowels;
+    for(let i = 1; i <= stringArray.length; i++){
+        if(isVowel(stringArray[i - 1])) currWindowVowels--;
+        if(isVowel(stringArray[i + k - 1])) currWindowVowels++;
+        maxVowels = currWindowVowels > maxVowels ? currWindowVowels : maxVowels
+    }
+
+    return maxVowels;
+};
